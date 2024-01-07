9e74b306064906c66ebdeee3d750493e4393af3e
Alan Cutter <randfur@gmail.com>
2024-01-08 09:28:18 +1100

    walk

--- 2024/julia-ship/main.js
+++ 2024/julia-ship/main.js
@@ -1,39 +1,18 @@
 import {Render} from './render.js';
-
-class Wanderer {
-  constructor() {
-    this.prevFrom = [0, 0, 0, 0];
-    this.from = [0, 0, 0, 0];
-    this.to = [0, 0, 0, 0];
-  }
-
-  push(v) {
-    this.prevFrom = this.from;
-    this.from = this.to;
-    this.to = v;
-  }
-
-  sample(progress) {
-
-  }
-}
-
-function smooth(x) {
-  return x <= 0.5 ? x ** 2 * 2 : 1 - ((1 - x) ** 2 * 2);
-}
+import {TrigWalk} from './trig-walk.js';
+import {SmoothRandomWalk} from './smooth-random-walk.js';
 
 async function main() {
   await Render.init();
 
+  const walk = SmoothRandomWalk;
+  // const walk = TrigWalk;
+  walk.init?.();
+
   while (true) {
     const time = await new Promise(requestAnimationFrame);
-    Render.render(
-      new Float32Array([
-        1 - 2 * Math.sin(time / 18000), 2, -3 + 8 * Math.cos(time / 8200), 4,
-        -8, -2 + 3 * Math.cos(time / 7350), 7, 4 - 6 * Math.sin(time / 21000),
-        -0.1, -0.3 + 0.5 * Math.sin(time / 12000), -1.2 + 0.1 * Math.cos(time / 23100), -0.35,
-      ]),
-    );
+    walk.update(time);
+    Render.render(walk.uniformData);
   }
 }
 
---
+++ 2024/julia-ship/smooth-random-walk.js
@@ -0,0 +1,139 @@
+export class SmoothRandomWalk {
+  static init() {
+    this.a = new Wanderer({
+      maxSteps: 410,
+      minSize: 0,
+      maxSize: 1,
+    });
+    this.b = new Wanderer({
+      maxSteps: 670,
+      minSize: 0,
+      maxSize: 1,
+    });
+    this.c = new Wanderer({
+      maxSteps: 1000,
+      minSize: 0.6,
+      maxSize: 1.2,
+    });
+  }
+
+  static update(time) {
+    for (const wanderer of [this.a, this.b, this.c]) {
+      wanderer.update();
+    }
+
+    this.uniformData = new Float32Array([
+      ...this.a.currentPoint.toArray(),
+      ...this.b.currentPoint.toArray(),
+      ...this.c.currentPoint.toArray(),
+    ]);
+  }
+}
+
+class Wanderer {
+  constructor({maxSteps, maxSize, minSize}) {
+    this.maxSteps = maxSteps;
+    this.maxSize = maxSize;
+    this.minSize = minSize;
+
+    this.prevFromPoint = this.randomVec4();
+    this.fromPoint = this.randomVec4();
+    this.toPoint = this.randomVec4();
+    this.currentPoint = null;
+    this.step = this.maxSteps;
+  }
+
+  randomVec4() {
+    const size = this.minSize + Math.random() * (this.maxSize - this.minSize);
+    const v = new Vec4(
+      deviate(1),
+      deviate(1),
+      deviate(1),
+      deviate(1),
+    );
+    return v.scale(size / v.length());
+  }
+
+  update() {
+    ++this.step;
+    if (this.step >= this.maxSteps) {
+      this.prevFromPoint = this.fromPoint;
+      this.fromPoint = this.toPoint;
+      this.toPoint = this.randomVec4();
+      this.step = 0;
+    }
+    const progress = this.step / this.maxSteps;
+    const projectedPoint = this.fromPoint.add(this.fromPoint.subtract(this.prevFromPoint).scale(progress));
+    const trajectoryPoint = this.fromPoint.add(this.toPoint.subtract(this.fromPoint).scale(progress));
+    this.currentPoint = projectedPoint.lerp(trajectoryPoint, smooth(progress));
+  }
+}
+
+function deviate(x) {
+  return (Math.random() * 2 - 1) * x;
+}
+
+function smooth(x) {
+  return x <= 0.5 ? x ** 2 * 2 : 1 - (1 - x) ** 2 * 2;
+}
+
+class Vec4 {
+  constructor(x=0, y=0, z=0, w=0) {
+    this.set(x, y, z, w);
+  }
+
+  set(x, y, z, w) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.w = w;
+  }
+
+  copy(v) {
+    this.set(v.x, v.y, v.z, v.w);
+  }
+
+  toArray() {
+    return [this.x, this.y, this.z, this.w];
+  }
+
+  length() {
+    return Math.sqrt(
+      this.x ** 2 +
+      this.y ** 2 +
+      this.z ** 2 +
+      this.w ** 2
+    );
+  }
+
+  add(v) {
+    return new Vec4(
+      this.x + v.x,
+      this.y + v.y,
+      this.z + v.z,
+      this.w + v.w,
+    );
+  }
+
+  subtract(v) {
+    return new Vec4(
+      this.x - v.x,
+      this.y - v.y,
+      this.z - v.z,
+      this.w - v.w,
+    );
+  }
+
+  scale(k) {
+    return new Vec4(
+      this.x * k,
+      this.y * k,
+      this.z * k,
+      this.w * k,
+    );
+  }
+
+  lerp(v, t) {
+    return this.scale(1 - t).add(v.scale(t));
+  }
+}
