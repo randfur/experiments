930cf3ff9d19263335aa7b76a71f090c7ea85e9e
Alan Cutter <randfur@gmail.com>
2024-01-07 21:55:34 +1100

    split

--- 2024/julia-ship/main.js
+++ 2024/julia-ship/main.js
@@ -1,121 +1,39 @@
-const width = window.innerWidth;
-const height = window.innerHeight;
-const canvas = document.createElement('canvas');
+import {Render} from './render.js';
+
+class Wanderer {
+  constructor() {
+    this.prevFrom = [0, 0, 0, 0];
+    this.from = [0, 0, 0, 0];
+    this.to = [0, 0, 0, 0];
+  }
+
+  push(v) {
+    this.prevFrom = this.from;
+    this.from = this.to;
+    this.to = v;
+  }
+
+  sample(progress) {
+
+  }
+}
+
+function smooth(x) {
+  return x <= 0.5 ? x ** 2 * 2 : 1 - ((1 - x) ** 2 * 2);
+}
 
 async function main() {
-  const device = await (await navigator.gpu.requestAdapter()).requestDevice();
-
-  canvas.width = width;
-  canvas.height = height;
-  document.body.append(canvas);
-  const context = canvas.getContext('webgpu');
-  context.configure({
-    device,
-    format: navigator.gpu.getPreferredCanvasFormat(),
-  });
-
-  const pipeline = device.createRenderPipeline({
-    layout: 'auto',
-    vertex: {
-      module: device.createShaderModule({
-        code: `
-        const vertices = array(
-          vec2f(-1, -1),
-          vec2f(-1, 1),
-          vec2f(1, -1),
-          vec2f(1, -1),
-          vec2f(-1, 1),
-          vec2f(1, 1),
-        );
-
-        struct Output {
-          @location(0) vertex: vec2f,
-          @builtin(position) position: vec4f,
-        }
-
-        @vertex
-        fn main(@builtin(vertex_index) vertex_index: u32) -> Output {
-          var output: Output;
-          output.vertex = vertices[vertex_index];
-          output.position = vec4f(output.vertex, 0, 1);
-          return output;
-        }
-        `,
-      }),
-      entryPoint: 'main',
-    },
-    fragment: {
-      module: device.createShaderModule({
-        code: `
-        // TODO: Replace with uniforms from main thread.
-        const a = vec4f(3, 4, -2, 9);
-        const b = vec4f(-1, 0, 1, 4);
-        // const c = vec4f(0.1, -0.7, 0.4, 0.2);
-        @group(0) @binding(0) var<uniform> c: vec4f;
-        const zoom = 0.3;
-
-        @fragment
-        fn main(@builtin(position) position: vec4f, @location(0) vertex: vec2f) -> @location(0) vec4f {
-          let xDir = normalize(a);
-          let yDir = normalize(b - xDir * dot(xDir, b));
-          let pixelPosition = c + (xDir * vertex.x + yDir * vertex.y) * zoom;
-
-          var z = pixelPosition.xy;
-          var c = pixelPosition.zw;
-          const maxCount: u32 = 100;
-          var count: u32 = 0;
-          for (var i: u32 = 0; i < maxCount; i += 1) {
-            z = vec2f(z.x * z.x - z.y * z.y, 2 * z.x * z.y) + c;
-            count = select(count, i, length(z) < 2);
-          }
-
-          return vec4f(vec3(1, 1, 1) * f32(count) / f32(maxCount), 1);
-        }
-        `,
-      }),
-      entryPoint: 'main',
-      targets: [{
-        format: navigator.gpu.getPreferredCanvasFormat(),
-      }],
-    },
-  });
-
-  const uniformBuffer = device.createBuffer({
-    size: 3 * 4 * 32 / 8,
-    usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
-  });
-
-  const uniformBindGroup = device.createBindGroup({
-    layout: pipeline.getBindGroupLayout(0),
-    entries: [{
-      binding: 0,
-      resource: {
-        buffer: uniformBuffer,
-      },
-    }],
-  });
+  await Render.init();
 
   while (true) {
     const time = await new Promise(requestAnimationFrame);
-    device.queue.writeBuffer(
-      uniformBuffer,
-      0,
-      new Float32Array([0.1, -0.5 + 0.5 * Math.sin(time / 10000), 0.4 + 0.1 * Math.cos(time / 6000), 0.2]),
+    Render.render(
+      new Float32Array([
+        1 - 2 * Math.sin(time / 18000), 2, -3 + 8 * Math.cos(time / 10000), 4,
+        -8, -2 + 3 * Math.cos(time / 43500), 7, 4 - 10 * Math.sin(time / 21000),
+        -0.3, -0.3 + 0.5 * Math.sin(time / 10000), 0.3 + 0.2 * Math.cos(time / 6000), 0.2,
+      ]),
     );
-
-    const commandEncoder = device.createCommandEncoder();
-    const renderPass = commandEncoder.beginRenderPass({
-      colorAttachments: [{
-        view: context.getCurrentTexture().createView(),
-        loadOp: 'clear',
-        storeOp: 'store',
-      }],
-    });
-    renderPass.setPipeline(pipeline);
-    renderPass.setBindGroup(0, uniformBindGroup);
-    renderPass.draw(6);
-    renderPass.end();
-    device.queue.submit([commandEncoder.finish()]);
   }
 }
 
---
+++ 2024/julia-ship/render.js
@@ -0,0 +1,124 @@
+export class Render {
+  static canvas;
+  static device;
+  static context;
+  static pipeline;
+  static uniformBuffer;
+  static uniformBindGroup;
+
+  static async init() {
+    this.canvas = document.createElement('canvas');
+    this.device = await (await navigator.gpu.requestAdapter()).requestDevice();
+
+    this.canvas.width = innerWidth;
+    this.canvas.height = innerHeight;
+    document.body.append(this.canvas);
+    this.context = this.canvas.getContext('webgpu');
+    this.context.configure({
+      device: this.device,
+      format: navigator.gpu.getPreferredCanvasFormat(),
+    });
+
+    this.pipeline = this.device.createRenderPipeline({
+      layout: 'auto',
+      vertex: {
+        module: this.device.createShaderModule({
+          code: `
+          const vertices = array(
+            vec2f(-1, -1),
+            vec2f(-1, 1),
+            vec2f(1, -1),
+            vec2f(1, -1),
+            vec2f(-1, 1),
+            vec2f(1, 1),
+          );
+
+          struct Output {
+            @location(0) vertex: vec2f,
+            @builtin(position) position: vec4f,
+          }
+
+          @vertex
+          fn main(@builtin(vertex_index) vertex_index: u32) -> Output {
+            var output: Output;
+            output.vertex = vertices[vertex_index];
+            output.position = vec4f(output.vertex, 0, 1);
+            return output;
+          }
+          `,
+        }),
+        entryPoint: 'main',
+      },
+      fragment: {
+        module: this.device.createShaderModule({
+          code: `
+          const xScale = ${Math.max(1, innerWidth / innerHeight)};
+          const yScale = ${Math.max(1, innerHeight / innerWidth)};
+          struct Uniforms {
+            a: vec4f,
+            b: vec4f,
+            c: vec4f,
+          };
+          @group(0) @binding(0) var<uniform> uniforms: Uniforms;
+          const zoom = 0.4;
+
+          @fragment
+          fn main(@builtin(position) position: vec4f, @location(0) vertex: vec2f) -> @location(0) vec4f {
+            let xDir = normalize(uniforms.a);
+            let yDir = normalize(uniforms.b - xDir * dot(xDir, uniforms.b));
+            let pixelPosition = uniforms.c + (xDir * vertex.x * xScale + yDir * vertex.y * yScale) * zoom;
+
+            var z = pixelPosition.xy;
+            var c = pixelPosition.zw;
+            const maxCount: u32 = 100;
+            var count: u32 = 0;
+            for (var i: u32 = 0; i < maxCount; i += 1) {
+              z = vec2f(z.x * z.x - z.y * z.y, 2 * z.x * z.y) + c;
+              count = select(count, i, length(z) < 2);
+            }
+
+            return vec4f(vec3(1, 1, 1) * f32(count) / f32(maxCount), 1);
+          }
+          `,
+        }),
+        entryPoint: 'main',
+        targets: [{
+          format: navigator.gpu.getPreferredCanvasFormat(),
+        }],
+      },
+    });
+
+    this.uniformBuffer = this.device.createBuffer({
+      size: 3 * 4 * 32 / 8,
+      usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+    });
+
+    this.uniformBindGroup = this.device.createBindGroup({
+      layout: this.pipeline.getBindGroupLayout(0),
+      entries: [{
+        binding: 0,
+        resource: {
+          buffer: this.uniformBuffer,
+        },
+      }],
+    });
+  }
+
+  static render(uniformData) {
+    this.device.queue.writeBuffer(this.uniformBuffer, 0, uniformData);
+
+    const commandEncoder = this.device.createCommandEncoder();
+    const renderPass = commandEncoder.beginRenderPass({
+      colorAttachments: [{
+        view: this.context.getCurrentTexture().createView(),
+        loadOp: 'clear',
+        storeOp: 'store',
+      }],
+    });
+    renderPass.setPipeline(this.pipeline);
+    renderPass.setBindGroup(0, this.uniformBindGroup);
+    renderPass.draw(6);
+    renderPass.end();
+    this.device.queue.submit([commandEncoder.finish()]);
+  }
+}
