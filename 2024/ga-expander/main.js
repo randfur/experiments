701653dcb412ff7b189555cabbcada5cac65b22d
Alan Cutter <alancutter@chromium.org>
2024-01-17 07:52:39 +1100

    barkbrak

--- 2024/ga-expander/main.js
+++ 2024/ga-expander/main.js
@@ -22,10 +22,70 @@ function main() {
   button.addEventListener('click', event => {
     output.textContent = parseAndExpand(textarea.textContent);
   });
+
+  document.body.append(parseAndExpand('abc = (4*def + ghi*X) * jkl*Y'));
 }
 
 main();
 
 function parseAndExpand(input) {
-  return 'dog';
+  const tokenTree = tokenise(input);
+  return tokenTree.join('|');
+}
+
+function tokenise(input) {
+  const tokens = [];
+  const stack = [];
+
+  let index = 0;
+
+  function consume(predicate) {
+    const start = index;
+    while (true) {
+      ++index;
+      if (index >= input.length || !predicate(index)) {
+        break;
+      }
+    }
+    return input.slice(start, index);
+  }
+
+  function next() {
+    return index < input.length ? input[index] : null;
+  }
+
+  while (true) {
+    if (index >= input.length) {
+      break;
+    }
+
+    if (isWhitespace(next())) {
+      consume(isWhitespace);
+      continue;
+    }
+
+    if (isAlpha(next())) {
+      const ident = consume(isAlphaNum);
+      if (next() === '(') {
+
+        stack.push(
+      }
+    } else if (c === '(') {
+    } else if (c === ')') {
+    }
+    ++index;
+  }
+
+  return tokens;
+}
+
+function isAlpha(char) {
+  return /[A-Za-z]/.test(char);
+}
+
+function isAlphaNum(char) {
+  return /[A-Za-z0-9]/.test(char);
+}
+function isWhitespace(char) {
+  return /\s/.test(char);
 }
