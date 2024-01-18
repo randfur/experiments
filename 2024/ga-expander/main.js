1d80e9d1f96790e076b7d64595300508d067472b
Alan Cutter <randfur@gmail.com>
2024-01-18 23:22:05 +1100

    tokeniser

--- 2024/ga-expander/main.js
+++ 2024/ga-expander/main.js
@@ -1,3 +1,5 @@
+import {tokenise} from './tokeniser.js';
+
 function main() {
   const textarea = document.createElement('textarea');
   textarea.style = `
@@ -23,101 +25,19 @@ function main() {
     output.textContent = parseAndExpand(textarea.textContent);
   });
 
-  document.body.append(parseAndExpand('abc = (4*def + ghi*X) * jkl*Y'));
+  document.body.append(parseAndExpand(`
+    abc = (4*def + ghi*X) * jkl*Y;
+    barkbark = abc * conjugate(abc);
+    barkbark
+  `));
 }
 
 main();
 
 function parseAndExpand(input) {
   const tokenTree = tokenise(input);
-  return tokenTree.join('|');
-}
-
-function tokenise(input) {
-  const stack = [{
-    ident: '',
-    children: [],
-  }];
-
-  let index = 0;
-
-  function consume(predicate) {
-    const start = index;
-    while (true) {
-      ++index;
-      if (index >= input.length || !predicate(index)) {
-        break;
-      }
-    }
-    return input.slice(start, index);
-  }
-  function next() {
-    return index < input.length ? input[index] : null;
-  }
-  function stackFrame() {
-    return stack[stack.length - 1];
-  }
-  function popStack() {
-    const frame = stackFrame();
-    stack.length -= 1;
-    return frame;
-  }
-
-  while (true) {
-    if (index >= input.length) {
-      break;
-    }
-
-    if (isWhitespace(next())) {
-      consume(isWhitespace);
-      continue;
-    }
-
-    if (isAlpha(next())) {
-      const ident = consume(isAlpha);
-      if (next() === '(') {
-        const group = {
-          ident,
-          children: [],
-        };
-        stack.push(group);
-        continue;
-      }
-
-      stackFrame.children.push({
-        ident,
-      });
-      continue;
-    }
-
-    if (next() === '(') {
-      const group = {
-        ident: '',
-        children: [],
-      };
-      stack.push(group);
-      continue;
-    }
-
-    if (isNumeric(next())) {
-      const number = consume(isNumeric);
-
-  }
-
-  if (stack.length !== 1) {
-    return ['bad braces'];
-  }
-  return stack[0];
-}
-
-function isAlpha(char) {
-  return /[A-Za-z]/.test(char);
-}
-
-function isNumeric(char) {
-  return /[A-Za-z0-9]/.test(char);
-}
-
-function isWhitespace(char) {
-  return /\s/.test(char);
+  console.log(tokenTree);
+  const astiser = astise(tokenTree);
+  console.log(ast);
+  return JSON.stringify(ast);
 }
---
+++ 2024/ga-expander/tokeniser.js
@@ -0,0 +1,98 @@
+export function tokenise(input) {
+  const stack = [{
+    children: [],
+  }];
+
+  let index = 0;
+
+  function consume(predicate) {
+    const start = index;
+    while (true) {
+      ++index;
+      if (index >= input.length || !predicate(input[index])) {
+        break;
+      }
+    }
+    return input.slice(start, index);
+  }
+  function next() {
+    return index < input.length ? input[index] : null;
+  }
+  function stackFrame() {
+    return stack[stack.length - 1];
+  }
+
+  while (true) {
+    if (index >= input.length) {
+      break;
+    }
+
+    if (isWhitespace(next())) {
+      consume(isWhitespace);
+      continue;
+    }
+
+    if (isAlpha(next())) {
+      const ident = consume(isAlpha);
+      if (next() === '(') {
+        stack.push({
+          ident,
+          children: [],
+        });
+        ++index;
+        continue;
+      }
+
+      stackFrame().children.push({
+        ident,
+      });
+      continue;
+    }
+
+    if (next() === '(') {
+      stack.push({
+        children: [],
+      });
+      ++index;
+      continue;
+    }
+
+    if (isNumeric(next())) {
+      stackFrame().children.push({
+        number: parseFloat(consume(isNumeric)),
+      });
+      continue;
+    }
+
+    if (next() === ')') {
+      const frame = stackFrame();
+      stack.length -= 1;
+      stackFrame().children.push(frame);
+      ++index;
+      continue;
+    }
+
+    stackFrame().children.push({
+      symbol: next(),
+    });
+    ++index;
+  }
+
+  if (stack.length !== 1) {
+    console.log(stack);
+    throw 'Bad braces';
+  }
+  return stack[0];
+}
+
+function isAlpha(char) {
+  return /[A-Za-z]/.test(char);
+}
+
+function isNumeric(char) {
+  return /[A-Za-z0-9\.]/.test(char);
+}
+
+function isWhitespace(char) {
+  return /\s/.test(char);
+}
