7c9e5d9b2cea1ff1ebd019d5fcde686bd9205334
Alan Cutter <randfur@gmail.com>
2024-02-17 12:03:18 +1100

    more save

--- 2024/gravity-veils/main.js
+++ 2024/gravity-veils/main.js
@@ -2,14 +2,17 @@ const f32Bytes = 4;
 const vec3Bytes = f32Bytes * 3;
 const vec4Bytes = f32Bytes * 4;
 
-const maxMassCount = 10;
-const maxParticleCount = 1000;
-const maxSimulationStepCount = 1000;
+const constants = {
+  maxMassCount: 10,
+  maxParticleCount: 1000,
+  maxSimulationStepCount: 1000,
+  simulationTimeStep: 1,
+};
 
 const massBytes = vec3Bytes + f32Bytes;
 const particleBytes = vec3Bytes + vec3Bytes + vec4Bytes;
 const trajectoryPointBytes = vec3Bytes + vec4Bytes;
-const trajectoryBytes = trajectoryPointBytes * maxSimulationStepCount;
+const trajectoryBytes = trajectoryPointBytes * constants.maxSimulationStepCount;
 
 async function main() {
   document.body.style = `
@@ -32,17 +35,17 @@ async function main() {
   });
 
   const massBuffer = device.createBuffer({
-    size: massBytes * maxMassCount,
+    size: massBytes * constants.maxMassCount,
     usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE,
   });
 
   const particleBuffer = device.createBuffer({
-    size: particleBytes * maxParticleCount,
+    size: particleBytes * constants.maxParticleCount,
     usage: GPUBufferUsage.COPY_DST | GPUBufferUsage.STORAGE,
   });
 
   const trajectoryBuffer = device.createBuffer({
-    size: trajectoryBytes * maxParticleCount,
+    size: trajectoryBytes * constants.maxParticleCount,
     usage: GPUBufferUsage.STORAGE | GPUBufferUsage.VERTEX,
   });
 
@@ -65,8 +68,8 @@ async function main() {
       }
 
       struct TrajectoryPoint {
-        vec3f position,
-        vec4f colour,
+        @location(0) vec3f position,
+        @location(1) vec4f colour,
       }
 
       alias Trajectory = array<TrajectoryPoint, maxSimulationStepCount + 1>;
@@ -78,7 +81,7 @@ async function main() {
       @compute @workgroup_size(64)
       fn simulate(@builtin(global_invocation_id) id: vec3u) {
         let particleIndex = id.x;
-        let particle = particles[particleIndex];
+        var particle = particles[particleIndex];
 
         trajectories[particleIndex][0] = TrajectoryPoint{
           position: particle.position,
@@ -87,9 +90,11 @@ async function main() {
 
         for (let i: u32 = 0; i < maxSimulationStepCount; ++i) {
           for (let j: u32 = 0; j < maxMassCount; ++j) {
-
+            let delta = masses[j].position - particle.position;
+            particle.velocity += delta * masses[j].size / dot(delta, delta) * simulationTimeStep;
           }
 
+          particle.position += particle.velocity * simulationTimeStep;
 
           trajectories[particleIndex][i + 1] = TrajectoryPoint{
             position: particle.position,
@@ -97,8 +102,64 @@ async function main() {
           };
         }
       }
+
+      struct Vertex {
+        @builtin(position) position: vec4f,
+        @location(0) colour: vec4f,
+      }
+
+      @vertex
+      fn vertex(trajectoryPoint: TrajectoryPoint) -> Vertex {
+        return Vertex{
+          position: vec4f(trajectoryPoint.position, 1),
+          colour: trajectoryPoint.colour,
+        };
+      }
+
+      @fragment
+      fn fragment(@location(0) colour: vec4f) -> @location(0) vec4f {
+        return colour;
+      }
     `,
   });
+
+  const simulationPipeline = device.createComputePipeline({
+    layout: 'auto',
+    compute: {
+      constants,
+      entryPoint: 'simulate',
+      module: shaderModule,
+    },
+  });
+
+  const storageBufferBindGroup = device.createBindGroup({
+    layout: simulationPipeline.getBindGroupLayout(0),
+    entries: [{
+      binding: 0,
+      resource: {
+        buffer: particleBuffer,
+      },
+    }, {
+      binding: 1,
+      resource: {
+        buffer: massBuffer,
+      },
+    }, {
+      binding: 2,
+      resource: {
+        buffer: trajectoryBuffer,
+      },
+    }],
+  });
+
+  const renderPipeline = device.createRenderPipeline({
+    layout: 'auto',
+    primitive: {
+      topology: 'triangle-strip',
+    },
+    vertex: {},
+    fragment: {},
+
 }
 
 main();
