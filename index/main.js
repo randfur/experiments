0e65f4fb2b7c14515062b55e05e287d20193a24e
Alan Cutter <randfur@gmail.com>
2025-09-27 21:53:19 +1000

    logo

---
+++ index/main.js
@@ -0,0 +1,241 @@
+const directory = {
+  2025: [
+    {name: 'seaweed', wip: false},
+    {name: 'cake-slices', wip: true},
+    {name: 'slanted-hexagons', wip: false},
+    {name: 'buildings', wip: false},
+    {name: 'linear-solver', wip: true},
+    {name: 'ice', wip: false},
+    {name: 'triangle-sphere', wip: false},
+    {name: 'pinch-zoom-debug', wip: false},
+    {name: 'bell-conducting', wip: false},
+    {name: 'plants', wip: true},
+    {name: 'gravity-veils', wip: false},
+  ],
+  2024: [
+    {name: 'tetris-scrabble', wip: true},
+    {name: 'bell-sequences', wip: true},
+    {name: 'json-json', wip: false},
+    {name: 'physics-of-life', wip: true},
+    {name: 'asteroids', wip: true},
+    {name: 'huffman-compression', wip: false},
+    {name: 'animate-hex-lines', wip: true},
+    {name: 'edit-distance', wip: false},
+    {name: 'radix-sort', wip: false},
+    {name: 'image-compositing', wip: true},
+    {name: 'translucent-triangle', wip: false},
+    {name: 'ollama-chat', wip: true},
+    {name: 'orbits', wip: false},
+    {name: 'transparent-pixelation-rendering', wip: false},
+    {name: 'gravity-veils', wip: false},
+    {name: 'ga-expander', wip: false},
+    {name: 'interpolate-points', wip: false},
+    {name: 'julia-ship', wip: false},
+  ],
+  2023: [
+    {name: 'grid-transform', wip: false},
+    {name: 'ember', wip: false},
+    {name: 'sudobiks-kube', wip: true},
+    {name: 'tic-tac-doku', wip: false},
+    {name: 'ga-quaternions', wip: true},
+    {name: '2048-rotate', wip: true},
+    {name: 'animate-hex-lines', wip: true},
+    {name: 'draw-hex-lines', wip: false},
+    {name: 'spirals', wip: false},
+    {name: 'webgl-example', wip: false},
+    {name: 'bomber-hex-lines', wip: false},
+    {name: 'html-input-event', wip: false},
+    {name: 'u32-to-f32-webgl', wip: false},
+    {name: 'hex-lines-webgl', wip: false},
+    {name: 'rotor', wip: true},
+    {name: 'bomber', wip: false},
+    {name: 'frame-edit-ui', wip: false},
+    {name: 'hex-lines-wgpu', wip: false},
+    {name: 'runes', wip: false},
+    {name: 'pet-language', wip: true},
+    {name: 'wgpu-merge-sort', wip: false},
+    {name: 'wgpu-example', wip: false},
+    {name: 'spinny-ground', wip: false},
+    {name: 'fill-tool', wip: false},
+    {name: '2d-matrix-transforms', wip: false},
+    {name: 'broadleaf-better-sock', wip: false},
+    {name: 'daffodil-puzzling-spectroscope', wip: false},
+    {name: 'forest-abrasive-regnosaurus', wip: false},
+    {name: 'giddy-abaft-marmoset', wip: false},
+    {name: 'lavender-pinto-jackfruit', wip: false},
+    {name: 'productive-pricey-creek', wip: false},
+  ],
+  2022: [
+    {name: 'deadpan-pepper-cave', wip: false},
+    {name: 'dear-brave-barber', wip: true},
+    {name: 'different-fan-marigold', wip: true},
+    {name: 'everlasting-marshy-plaster', wip: false},
+    {name: 'everlasting-patch-situation', wip: true},
+    {name: 'excellent-pepper-brie', wip: true},
+    {name: 'fierce-aromatic-mail', wip: true},
+    {name: 'glaze-horn-aurora', wip: false},
+    {name: 'good-peat-submarine', wip: false},
+    {name: 'gravel-rich-neighbor', wip: true},
+    {name: 'hot-spurious-zebu', wip: false},
+    {name: 'ink-thunder-protest', wip: true},
+    {name: 'loud-chestnut-game', wip: true},
+    {name: 'luminous-cobalt-peripheral', wip: false},
+    {name: 'oasis-caramel-sturgeon', wip: false},
+    {name: 'plausible-rowan-utensil', wip: false},
+    {name: 'storm-wild-garment', wip: false},
+    {name: 'tame-plastic-coriander', wip: false},
+    {name: 'tin-flaxen-beech', wip: false},
+    {name: 'well-hallowed-mousepad', wip: false},
+    {name: 'working-dynamic-cup', wip: false},
+  ],
+  2021: [
+    {name: 'ablaze-abundant-jump', wip: false},
+    {name: 'aboard-malachite-double', wip: false},
+    {name: 'audiomedic', wip: true},
+    {name: 'cherry-bird-radish', wip: true},
+    {name: 'colorful-exuberant-echinodon', wip: false},
+    {name: 'flawless-glacier-ringer', wip: false},
+    {name: 'gilded-thirsty-chauffeur', wip: false},
+    {name: 'humble-pear-residue', wip: false},
+    {name: 'lapis-chartreuse-scaffold', wip: false},
+    {name: 'married-gravel-dewberry', wip: true},
+    {name: 'merciful-equal-porpoise', wip: false},
+    {name: 'mirage-electric-sprite', wip: false},
+    {name: 'nosy-iridescent-rotate', wip: true},
+    {name: 'notes-graph', wip: false},
+    {name: 'omniscient-intermediate-gallon', wip: false},
+    {name: 'shore-defiant-bandana', wip: false},
+    {name: 'shrub-pond-taleggio', wip: false},
+    {name: 'sinful-lunar-cereal', wip: true},
+    {name: 'spiffy-sepia-guilty', wip: false},
+    {name: 'subsequent-gold-gigantoraptor', wip: true},
+    {name: 'teal-brass-spur', wip: false},
+    {name: 'tested-calico-vise', wip: false},
+    {name: 'versed-silky-garment', wip: false},
+    {name: 'wiry-traveling-krill', wip: false},
+    {name: 'yellow-spiny-lute', wip: false},
+  ],
+  2020: [
+    {name: 'absolute-meerkat', wip: false},
+    {name: 'aimless-venus', wip: true},
+    {name: 'amber-truth-innovation', wip: false},
+    {name: 'ash-swordtail', wip: false},
+    {name: 'bony-ceramics', wip: true},
+    {name: 'crimson-shadow-rise', wip: false},
+    {name: 'dramatic-functional-belt', wip: false},
+    {name: 'fluffy-subdued-virgo', wip: false},
+    {name: 'fortune-lowly-saw', wip: false},
+    {name: 'hickory-holy-scissor', wip: true},
+    {name: 'luminous-gainful-carp', wip: true},
+    {name: 'mercurial-seen-seahorse', wip: true},
+    {name: 'obtainable-absorbing-sovereign', wip: false},
+    {name: 'plaid-pomegranate', wip: false},
+    {name: 'pleasant-chauffeur', wip: true},
+    {name: 'political-juicy-bag', wip: false},
+    {name: 'shaded-buttercup-lemonade', wip: true},
+    {name: 'stingy-harmonious-lawyer', wip: true},
+    {name: 'straw-summer', wip: true},
+    {name: 'stump-tungsten-mink', wip: false},
+    {name: 'unicode-explorer', wip: false},
+    {name: 'wax-obvious-pedigree', wip: false},
+    {name: 'witty-legend-velociraptor', wip: true},
+    {name: 'yellow-spruce', wip: true},
+    {name: 'youtube-buckets', wip: true},
+    {name: 'zany-natural-filth', wip: false},
+  ],
+  2019: [
+    {name: 'catkin-paprika', wip: true},
+    {name: 'deeply-amphibian', wip: false},
+    {name: 'even-saffron', wip: true},
+    {name: 'honorable-oboe', wip: false},
+    {name: 'industrious-creek', wip: false},
+    {name: 'iridescent-hisser', wip: false},
+    {name: 'puddle-molecule', wip: false},
+    {name: 'seemly-meter', wip: false},
+    {name: 'titanium-pyramid', wip: false},
+    {name: 'universal-pulsar', wip: true},
+    {name: 'youthful-macaroon', wip: true},
+    {name: 'zealous-pail', wip: false},
+  ],
+  2018: [
+    {name: 'discreet-hook', wip: false},
+    {name: 'familiar-violet', wip: false},
+    {name: 'four-venom', wip: false},
+    {name: 'lime-okra', wip: false},
+    {name: 'ripe-raven', wip: false},
+    {name: 'safe-flame', wip: false},
+  ],
+};
+
+function main() {
+  document.body.innerHTML = `
+    <img id="logo" src="index/favicon.png">
+    <h1>
+      <a href="https://github.com/randfur/experiments">randfur experiments</a>
+    </h1>
+    ${Object.entries(directory)
+      .sort(([keyA, valueA], [keyB, valueB]) => keyB - keyA)
+      .map(([year, experiments]) => {
+        const {false: readyExperiments, true: wipExperiments} = groupBy(experiments, 'wip');
+        return `
+          <h2>
+            <a href="https://github.com/randfur/experiments/tree/main/${year}">${year}</a>
+          </h2>
+          ${
+            readyExperiments && readyExperiments.length > 0
+            ? `<ul>
+                ${readyExperiments.map(experiment => renderExperiment(year, experiment)).join('')}
+              </ul>
+            `
+            : ''
+          }
+          ${
+            wipExperiments && wipExperiments.length > 0
+            ? `<div class="collapsable-container collapsed">
+                <button class="collapse-toggle" onclick="this.parentElement.classList.toggle('collapsed')">
+                  unfinished experiments
+                  <div class="collapse-turnstyle">△</div>
+                </button>
+                <ul class="collapsing-list">
+                  ${wipExperiments.map(experiment => renderExperiment(year, experiment)).join('')}
+                </ul>
+              </div>
+            `
+            : ''
+          }
+        `;
+      })
+      .join('')
+    }`;
+}
+
+function renderExperiment(year, experiment) {
+  return `
+    <li>
+      <a href="${year}/${experiment.name}">
+        <div class="main-link">
+          <img class="thumbnail" src="${year}/${experiment.name}/thumbnail.png" onerror="this.src='index/wip.png'">
+          <div class="experiment-name">${experiment.name}</div>
+        </div>
+      </a>
+      <div class="sub-links">
+        <a href="${year}/${experiment.name}/notes.txt">notes</a>
+        |
+        <a href="https://github.com/randfur/experiments/tree/main/${year}/${experiment.name}">src</a>
+      </div>
+    </li>
+  `;
+}
+
+function groupBy(list, key) {
+  const result = {};
+  for (const item of list) {
+    if (!(item[key] in result)) {
+      result[item[key]] = [];
+    }
+    result[item[key]].push(item);
+  }
+  return result;
+}
+
+main();
