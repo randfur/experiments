87a142c1d26fa0e5ffaf30de85906ca5697e8e39
Alan Cutter <randfur@gmail.com>
2026-07-06 00:02:56 +1000

    camera-speed

--- 2026/wormhole/main.js
+++ 2026/wormhole/main.js
@@ -8,60 +8,118 @@ const TAU = Math.PI * 2;
 async function main() {
   const {hexLinesContext} = HexLinesContext.setupFullPageContext({is3d: true});
   const hexLines = hexLinesContext.createLines();
-  const path = new Path();
-  const camera = new Camera(path);
+  const path = new Path(1000);
 
   while (true) {
     await new Promise(requestAnimationFrame);
-    path.step();
+    path.progress(10);
     hexLines.clear();
     path.render(hexLines);
     Mat4.multiply(
-      Mat4.a.setRotateRotor3(Rotor3.conjugate(path.segments[0].orientation)),
-      Mat4.b.setTranslateVec3(Vec3.scale(-1, path.segments[0].position)),
+      Mat4.a.setRotateRotor3(Rotor3.conjugate(path.start.orientation)),
+      Mat4.b.setTranslateVec3(Vec3.scale(-1, path.start.position)),
     ).exportToArrayBuffer(hexLines.transformMatrix);
     hexLines.draw();
   }
 }
 
 class Path {
-  constructor() {
+  constructor(pointCount) {
     this.stepCount = 0;
-    this.segments = [];
-    this.targetSegmentCount = 1000;
-    this.position = new Vec3();
-    this.orientation = new Rotor3();
-  }
 
-  step() {
-    this.segments.shift();
-    while (this.segments.length < this.targetSegmentCount) {
-      ++this.stepCount;
-      this.orientation.inplaceMultiplyLeft(Rotor3.axisAngle(Vec3.polar((this.stepCount) / 40), 0.01));
-      this.position.inplaceAdd(Vec3.z().inplaceRotateRotor3(this.orientation).inplaceScale(10));
-      this.segments.push(new Segment(this.position.clone(), this.orientation.clone()));
+    this.pointCount = pointCount;
+    this.startIndex = 0;
+
+    this.start = new PathPoint();
+    this.end = new PathPoint();
+
+    this.points = [];
+    for (let i = 0; i < this.pointCount; ++i) {
+      this.points.push(new PathPoint());
+      this.writeNextPoint(i);
     }
   }
 
+  progress(distance) {
+    let remainingDistance = distance;
+    let previousDistance = this.start.distance;
+    let deleteCount = 0;
+    for (let i = 0; i < this.pointCount; ++i) {
+      const point = this.points[(this.startIndex + i) % this.pointCount];
+      const segmentDistance = point.distance - previousDistance;
+      if (remainingDistance > segmentDistance) {
+        remainingDistance -= segmentDistance;
+        ++deleteCount;
+      } else {
+        break;
+      }
+    }
+
+    if (deleteCount >= this.pointCount) {
+      this.startIndex = 0;
+      this.start = new PathPoint();
+      this.end = new PathPoint();
+      for (let i = 0; i < deleteCount; ++i) {
+        this.writeNextPoint(i);
+      }
+      return;
+    }
+
+    if (deleteCount > 0) {
+      this.start.set(this.points[(this.startIndex + deleteCount - 1) % this.pointCount]);
+    }
+    for (let i = 0; i < deleteCount; ++i) {
+      this.writeNextPoint(this.startIndex)
+      this.startIndex = (this.startIndex + 1) % this.pointCount;
+    }
+
+    const startPoint = this.points[this.startIndex];
+    const overShoot = remainingDistance / (startPoint.distance - this.start.distance);
+    this.start.distance = lerp(this.start.distance, startPoint.distance, overShoot);
+    this.start.position.inplaceLerp(startPoint.position, overShoot);
+    this.start.orientation.inplaceLerp(startPoint.orientation, overShoot).inplaceNormalise();
+  }
+
+  writeNextPoint(index) {
+    ++this.stepCount;
+    this.end.orientation.inplaceMultiplyLeft(
+      Rotor3.axisAngle(
+        Vec3.polar((this.stepCount) / 40),
+        0.01,
+      ),
+    );
+    const stepDistance = 10;
+    this.end.distance += stepDistance;
+    this.end.position.inplaceAdd(Vec3.z().inplaceRotateRotor3(this.end.orientation).inplaceScale(stepDistance));
+    this.points[index].set(this.end);
+  }
+
   render(hexLines) {
-    for (const segment of this.segments) {
-      segment.render(hexLines);
+    for (const point of this.points) {
+      point.render(hexLines);
     }
   }
 }
 
-class Segment {
-  constructor(position, orientation) {
-    this.position = position;
-    this.orientation = orientation;
+class PathPoint {
+  constructor() {
+    this.distance = 0;
+    this.position = new Vec3();
+    this.orientation = new Rotor3();
+  }
+
+  set(other) {
+    this.distance = other.distance;
+    this.position.set(other.position);
+    this.orientation.set(other.orientation);
   }
 
   render(hexLines) {
-    const ringSegmentCount = 10;
+    const ringSegmentCount = 6;
     const ringRadius = 100;
     for (let i = 0; i <= ringSegmentCount; ++i) {
       hexLines.addPoint({
-        position: Vec3.polar(i / ringSegmentCount * TAU, ringRadius).inplaceRotateRotor3(this.orientation).inplaceAdd(this.position),
+        position: Vec3.polar((i / ringSegmentCount) * TAU, ringRadius).inplaceRotateRotor3(this.orientation).inplaceAdd(this.position),
         size: 1,
         colour: {r: 255, g: 255, b: 255},
       });
@@ -70,7 +128,8 @@ class Segment {
   }
 }
 
-class Camera {
+function lerp(a, b, t) {
+  return a + (b - a) * t;
 }
 
 main();
